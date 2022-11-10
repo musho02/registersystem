8c1c26f7aa719ff9e6ef370ebdb0be1c782524af
musho02 <109723460+musho02@users.noreply.github.com>
2022-11-10 11:23:29 +0700

    Update login_db.php

--- User/login_db.php
+++ User/login_db.php
@@ -1,10 +1,12 @@
 <?php 
+    include('db_connect.php');
     session_start();
     $errors = array();
 
 if(isset($_POST['login_user'])){
-    $username = mysqli_real_escape_string($conn ,$_POST['username']);
-    $password = mysqli_real_escape_string($conn ,$_POST['password']);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $passwordenc = md5($password);
 
     if(empty($username)){
         array_push($errors, "Username is required");
@@ -14,17 +16,24 @@ if(isset($_POST['login_user'])){
     }
     if(count($errors) == 0){
         $password = md5($password);
-        $query = "SELECT * FROM User WHERE username = '$username' AND password = '$password'";
+        $query = "SELECT * FROM user WHERE username = '$username' AND password = '$passwordenc'";
         $result = mysqli_query($conn ,$query);
 
         if(mysqli_num_rows($result) == 1){
             $_SESSION['username'] = $username;
             $_SESSION['success'] = "Login successful";
-            header("location: index.php");
+            header("location: user_page.php");
         } else {
-            array_push($errors ,"Username or password is wrong")
+            array_push($errors ,"Username or password is wrong");
             $_SESSION['error'] = "Username or password is wrong";
             header("location: login.php");
+        }
+
+        if($_SESSION['status'] == 'a'){
+            header("location: admin_page.php");
+        }
+        if($_SESSION['status'] == 'u'){
+            header("location: user_page.php")
         }
     } 
 }
